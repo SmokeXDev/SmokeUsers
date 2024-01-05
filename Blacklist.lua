33bc0db3f89dbcb82757e392f0cb719b48e6609c
SmokeXDev <111679379+SmokeXDev@users.noreply.github.com>
2024-01-05 20:09:26 +0100

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -51,37 +51,31 @@ for playerName, kickInfo in pairs(blacklisted) do
     if blacklistedplr == Players.LocalPlayer then
         if kickInfo.BadWords == true then
             if game:GetService("TextChatService").ChatVersion == Enum.ChatVersion.TextChatService then
-                while true do
-                    task.wait()
-                    for _, BadWord in ipairs(BadWords) do
-                        for i = 1, kickInfo.SpamCount or 3 do
-                            game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(BadWord)
-                            task.wait(kickInfo.KickDelay or 1)
-                        end
+                for _, BadWord in ipairs(BadWords) do
+                    for i = 1, kickInfo.SpamCount or 3 do
+                        game:GetService("TextChatService").ChatInputBarConfiguration.TargetTextChannel:SendAsync(BadWord)
+                        task.wait(.3)
                     end
-
-                    task.wait(kickInfo.KickDelay or 5)
-
-                    Players.LocalPlayer:Kick(kickInfo.Reason)
-                    setfpscap(1)
-                    Teleport:Teleport(kickInfo.GameTP)
                 end
+
+                task.wait(kickInfo.KickDelay or 5)
+
+                Players.LocalPlayer:Kick(kickInfo.Reason)
+                setfpscap(1)
+                Teleport:Teleport(kickInfo.GameTP)
             elseif game:GetService("TextChatService").ChatVersion == Enum.ChatVersion.LegacyChatService then
-                while true do
-                    task.wait()
-                    for _, BadWord in ipairs(BadWords) do
-                        for i = 1, kickInfo.SpamCount or 3 do
-                            ReplicatedStorage:WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(BadWord, "All")
-                            task.wait(kickInfo.KickDelay or 1)
-                        end
+                for _, BadWord in ipairs(BadWords) do
+                    for i = 1, kickInfo.SpamCount or 3 do
+                        ReplicatedStorage:WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest"):FireServer(BadWord, "All")
+                        task.wait(.3)
                     end
-
-                    task.wait(kickInfo.KickDelay or 5)
-
-                    Players.LocalPlayer:Kick(kickInfo.Reason)
-                    setfpscap(1)
-                    Teleport:Teleport(kickInfo.GameTP)
                 end
+
+                task.wait(kickInfo.KickDelay or 5)
+
+                Players.LocalPlayer:Kick(kickInfo.Reason)
+                setfpscap(1)
+                Teleport:Teleport(kickInfo.GameTP)
             end
         end
     end
